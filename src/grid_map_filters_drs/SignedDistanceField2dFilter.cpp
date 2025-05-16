abf8e38207509a05f871056c1ce4414c9eaaca89
jurichter <jurichter@leggedrobotics.com>
2025-05-16 07:58:33 +0000

    uncommenting dilation

--- src/grid_map_filters_drs/SignedDistanceField2dFilter.cpp
+++ src/grid_map_filters_drs/SignedDistanceField2dFilter.cpp
@@ -92,11 +92,11 @@ bool SignedDistanceField2dFilter<T>::update(const T& mapIn, T& mapOut) {
   cv::bitwise_not(cvObstacleSpaceMask, cvFreeSpaceMask);
 
   // Inflate obstacles (dilate the obstacle mask)
-  int inflation_pixels = 40; // <-- Set this to the number of pixels to inflate
-  cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2*inflation_pixels+1, 2*inflation_pixels+1));
-  cv::dilate(cvObstacleSpaceMask, cvObstacleSpaceMask, kernel);
-  // Optionally, update free space mask as well
-  cv::bitwise_not(cvObstacleSpaceMask, cvFreeSpaceMask);
+  // int inflation_pixels = 40; // <-- Set this to the number of pixels to inflate
+  // cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2*inflation_pixels+1, 2*inflation_pixels+1));
+  // cv::dilate(cvObstacleSpaceMask, cvObstacleSpaceMask, kernel);
+  // // Optionally, update free space mask as well
+  // cv::bitwise_not(cvObstacleSpaceMask, cvFreeSpaceMask);
   profiler_ptr_->endEvent("1.preprocess");
 
   // Compute SDF
